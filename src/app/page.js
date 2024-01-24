23949bb0a254ad9d7daa372aa889a6434548924d
Tikron <d.hahn.gb@gmail.com>
2024-01-24 17:52:46 +0000

    initial git upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,33 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { clearPreviewData } from "next/dist/server/api-utils";
 
 export default function Home() {
+  const collection = [
+    { name: "Bitoku", price: "45", extras: "insert", boughtNew: false },
+    {
+      name: "Villagers",
+      price: "50",
+      extras: "wooden coins, expansions",
+      boughtNew: false,
+    },
+    { name: "Apiary", price: "45", extras: "none", boughtNew: false },
+    { name: "Aquatica", price: "45", extras: "Cold Waters", boughtNew: false },
+  ];
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <h2>Board Games</h2>
+      {collection.map((collection) => {
+        return (
+          <div key={collection.name} className="post">
+            <h4>{collection.name}</h4>
+            <div className="detail">
+              <p>Price: £{collection.price}</p>
+              <p>Extras: {collection.extras}</p>
+              <p>Bought: {collection.boughtNew ? "New" : "Used"}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h1>My Collection</h1>
+    </div>
+  );
+}
